156aa0ec2409ba877d96cb13dd1e697c41f2156a
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-06-19 21:44:47 -0300

    💄 feat: Header CSS Styling

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,14 @@
 export default function Header() {
     return (
-        <header className="bg-purple-700 text-white p-4">
-            <h1>Header component</h1>
-        </header>
+        <main className="flex flex-col">
+            <header className="flex items-center justify-center bg-[#4F372F] text-white h-16 z-10">
+                <h1>Menu</h1>
+            </header>
+            <div className="relative flex items-center justify-center w-full h-[150px] -z-0 overflow-hidden">
+                <img src="public/assets/burger.jpeg" alt="burger" className="absolute w-full h-full scale-[200%] opacity-90 object-cover"/>
+                <img src="public/assets/logo.png" alt="logo" className="absolute w-52 opacity-85"/>
+
+            </div>
+        </main>
     );
 }
