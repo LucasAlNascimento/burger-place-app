5b01d93292b598e2afd2ab4a1a13394f7593e094
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-06-24 10:37:56 -0300

    ♻️ refactor: Removing React Router
    Now, the project don't use anymore React Router

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,18 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
 import Home from './containers/Home/home'
 
 import './styles.css'
 
 import { PersistGate } from 'redux-persist/integration/react'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-]);
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <Home />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
