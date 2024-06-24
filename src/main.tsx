dafc913006e050c99d6fab13ba325dd24f6c121c
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-06-24 11:23:04 -0300

    Revert "♻️ refactor: Removing React Router"
    This reverts commit 5b01d93292b598e2afd2ab4a1a13394f7593e094.

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,27 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Home from './containers/Home/home'
 
 import './styles.css'
 
 import { PersistGate } from 'redux-persist/integration/react'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Home />
+        <RouterProvider router={router} />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
